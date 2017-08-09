e94a38e9c612ffd5123b549b59eff5f155b1d4d1
Resten <saup19@naver.com>
2017-08-09 22:17:35 +0900

    loadActivity 추가

---
+++ app/src/main/java/com/example/resten/smartspace_project/LoadActivity.java
@@ -0,0 +1,17 @@
+package com.example.resten.smartspace_project;
+
+/**
+ * Created by Resten on 2017-08-09.
+ */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class LoadActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.load_xml);
+    }
+
+}
