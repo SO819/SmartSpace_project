ccadd522b726991a6ad6120fa55f52dfdb77c335
Resten <saup19@naver.com>
2017-08-11 13:28:37 +0900

    title_bar 제거

--- app/src/main/java/com/example/resten/smartspace_project/LoadActivity.java
+++ app/src/main/java/com/example/resten/smartspace_project/LoadActivity.java
@@ -6,13 +6,16 @@ package com.example.resten.smartspace_project;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.Window;
+
 
 public class LoadActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.load_xml);
+
     }
 
 }
