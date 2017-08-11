d346cd1f0a0d5b9c578a893ae0843747f04591bb
Resten <saup19@naver.com>
2017-08-11 12:48:49 +0900

    background 추가

--- app/src/main/java/com/example/resten/smartspace_project/LoadActivity.java
+++ app/src/main/java/com/example/resten/smartspace_project/LoadActivity.java
@@ -11,6 +11,7 @@ public class LoadActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.load_xml);
     }
 
